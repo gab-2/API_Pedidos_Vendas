24baaa502ea0a8220ce51988c363d5f5d8172607
gab-2 <107018299+gab-2@users.noreply.github.com>
2024-04-19 20:37:36 -0300

    Alterações nas filas

--- Fila/Program.cs
+++ Fila/Program.cs
@@ -1,22 +1,18 @@
-﻿using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Amqp.Framing;
 string connectionString = "Endpoint=sb://mensagens-unirv.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=01eFcbPQ6b7q2sfsebiVfuXtpw3hGvZKy+ASbOJlPLI=";
+
 string fila = "notificacao_faturamento";
-
 var client = new ServiceBusClient(connectionString);
 
-
 ServiceBusSender sender = client.CreateSender(fila);
-ServiceBusMessage message = new ServiceBusMessage("Japones Safado");
+ServiceBusMessage message = new ServiceBusMessage("Notificação de Faturamento");
 await sender.SendMessageAsync(message);
 
 
-string connectionString = "Endpoint=sb://mensagens-unirv.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=01eFcbPQ6b7q2sfsebiVfuXtpw3hGvZKy+ASbOJlPLI=";
-string fila = "notificar_expedicaoo";
+string fila2 = "notificar_expedicaoo";
+var client2 = new ServiceBusClient(connectionString);
 
-var client = new ServiceBusClient(connectionString);
-
-
-ServiceBusSender sender = client.CreateSender(fila);
-ServiceBusMessage message = new ServiceBusMessage("Japones Safado");
-await sender.SendMessageAsync(message);
+ServiceBusSender sender2 = client2.CreateSender(fila2);
+ServiceBusMessage message2 = new ServiceBusMessage("Noficação de Expedição");
+await sender.SendMessageAsync(message2);
--- obj/Debug/net8.0/API_PEDIDOS_VENDAS.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_PEDIDOS_VENDAS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_PEDIDOS_VENDAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a79cdea1732ff57e3a0943a80b05e9163ff5a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+596cd3f4dcdb0b91ea99f02cb82689b533c594c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_PEDIDOS_VENDAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_PEDIDOS_VENDAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
